a706bcbf31f7c1d6a61f4bc2c32f8d491a72c7d7
Adarsh R <adarshofficial268@gmail.com>
2025-03-01 21:23:55 +0530

    commit

--- Loan_Management_System/wsgi.py
+++ Loan_Management_System/wsgi.py
@@ -5,3 +5,8 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Loan_Management_System.settings')
 
 application = get_wsgi_application()
+
+
+from django.core.management import call_command
+call_command('migrate')
+call_command('createsuperuser', interactive=False, email='admin@example.com', username='admin')
