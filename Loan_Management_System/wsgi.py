0f4045fa3068717deffa5d67c35aa59c4ef16733
ADARSH R <114325928+AdarshR268@users.noreply.github.com>
2025-03-02 07:19:06 +0530

    Update wsgi.py

--- Loan_Management_System/wsgi.py
+++ Loan_Management_System/wsgi.py
@@ -7,8 +7,3 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Loan_Management_System.settings
 application = get_wsgi_application()
 
 
-try:
-    call_command('migrate')  # Apply migrations
-    call_command('createsuperuser', '--no-input', email='admin@example.com')  # Create superuser
-except Exception as e:
-    print(f"Error during startup: {e}")
